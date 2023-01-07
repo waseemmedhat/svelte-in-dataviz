0228bf81fd1265da3839b158a240b126a7d0c0f5
waseemmedhat <waseem.medhat@clin-pro.com>
2023-01-07 17:42:27 +0200

    page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
+	import { fade } from 'svelte/transition';
 	import '../app.css';
+
+	export let data;
 </script>
 
 <div class="container">
-	<slot />
+	{#key data.url}
+		<div in:fade={{ delay: 300, duration: 250 }} out:fade={{ delay: 0, duration: 250 }}>
+			<slot />
+		</div>
+	{/key}
 </div>
