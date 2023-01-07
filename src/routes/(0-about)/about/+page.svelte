c059087856dc9929798df010a797b7ff3d8c660f
waseemmedhat <waseem.medhat@clin-pro.com>
2023-01-07 17:31:04 +0200

    project restructure

---
+++ src/routes/(0-about)/about/+page.svelte
@@ -0,0 +1,42 @@
+<h2 class="mb center">About this project</h2>
+
+<p class="mb justify">
+	So, I came up with this way to escape tutorial hell and to do more practical stuff but without
+	actually leaving the tutorials. This will be my way of practicing with Svelte, CSS, and data
+	visualization.
+</p>
+
+<p class="mb justify">
+	The main idea is to take the <a href="https://svelte.dev/tutorial/">official Svelte tutorial</a>
+	and to apply each tutorial lesson on a dataviz-related topic. I have no idea on how to implement this
+	into an actual project, but I took the leap to start anyway and see where the project will go from
+	there.
+</p>
+
+<p class="mb center">
+	<img class="mb" src="/michael.jpg" alt="Michael Scott starts a sentence" />
+</p>
+
+<h2 class="mb center">Technologies</h2>
+<ul class="mb">
+	<li><b>Svelte</b>: what I'm here for.</li>
+	<li>
+		<b>SvelteKit</b>: I use it here mainly as a SSG with <code>adapter-static</code>, so I won't use
+		any fancy features, just taking advantage of client-side routing and general project structure.
+	</li>
+	<li>
+		<b>CSS</b>: I almost used Tailwind for this project, but thought I'd try just vanilla CSS to
+		maximize the learning I get out of this project
+	</li>
+	<li>
+		<b>D3</b>: This is the de facto standard library for data visualization. It provides low-level
+		features and utilities for building visualizations. It also has a DOM manipulation API similar
+		to jQuery, but I will hand that over to Svelte.
+	</li>
+</ul>
+
+<p class="mb justify">
+	<i>Note:</i> The styling of this project (font, colors, etc.) is heavily inspired from the
+	<a href="https://svelte.dev/">Official Svelte web page</a>, though I might not have done a great
+	job at imitating the design.
+</p>
